efff7f3aaf5239e866ef4728286554aad4e073b6
tristaavrilia <tristaavrilia@gmail.com>
2025-06-04 15:35:51 +0700

    tambah view

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtzxluD-670lfHCbbqweVayAPIx6CNe0o",
+  authDomain: "devops-trista.firebaseapp.com",
+  projectId: "devops-trista",
+  storageBucket: "devops-trista.firebasestorage.app",
+  messagingSenderId: "164514167755",
+  appId: "1:164514167755:web:04733beb62663b782d544e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
